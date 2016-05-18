9e716701f620d4ff65bebc4c64438b8f7f8390f7
皓皓P <HaoHaoP@HaoHaoP.com>
2016-05-18 18:52:44 +0800

    Update font.js

--- js/font.js
+++ js/font.js
@@ -1,3 +1,5 @@
-$youziku.load("#h2", "d0071c3c83274becaad7b00c9928cb7b", "DroidSans"); 
-$youziku.load("#p", "d0071c3c83274becaad7b00c9928cb7b", "DroidSans"); 
+$youziku.load("#intro", "d0071c3c83274becaad7b00c9928cb7b", "DroidSans"); 
+$youziku.load("#one", "d0071c3c83274becaad7b00c9928cb7b", "DroidSans"); 
+$youziku.load("#two", "d0071c3c83274becaad7b00c9928cb7b", "DroidSans"); 
+$youziku.load("#work", "d0071c3c83274becaad7b00c9928cb7b", "DroidSans"); 
 $youziku.draw();
